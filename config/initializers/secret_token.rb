5c0b3ab5b1072e69b5678ce75edb5388ec8d2adc
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2013-06-13 11:37:59 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VillageOfHope::Application.config.secret_token = '0dd842397f87a84d1ed714511c9c9098e68c84584c986f8eb0dd75c721a0d878fc0324ba5ac22427db9a2c5148079673a25659689926a9c86ca9d70093f3dc7e'
